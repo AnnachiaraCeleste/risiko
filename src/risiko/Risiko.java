fa8c072a3edcd5ecc61dd78ecee8f80c4932abbf
AnnachiaraCeleste <annachiaraceleste@gmail.com>
2024-01-15 20:26:16 +0100

    anna

--- src/risiko/Risiko.java
+++ src/risiko/Risiko.java
@@ -15,7 +15,8 @@ public class Risiko {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //prova per vedere se funziona
+        //prova per vedere se funziona 
+        //prova per provare
     }
     
 }
