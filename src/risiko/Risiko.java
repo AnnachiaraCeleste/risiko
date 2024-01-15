8601c5eefcb6ff9b8282e2c20381683f757aa79d
AnnachiaraCeleste <134926246+AnnachiaraCeleste@users.noreply.github.com>
2024-01-15 20:00:22 +0100

    Update Risiko.java

--- src/risiko/Risiko.java
+++ src/risiko/Risiko.java
@@ -15,7 +15,7 @@ public class Risiko {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //prova per vedere se funziona
     }
     
 }
